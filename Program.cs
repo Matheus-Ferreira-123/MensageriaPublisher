3ee28e6e77480487aa418aa23df4944a247e15b7
Matheus-Ferreira-123 <matheuslopesferreirabh@gmail.com>
2025-06-17 10:08:06 -0300

    Projeto publisher com RabbitMQ

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "fila1",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+string message = "Olá, mensagem de teste para o consumer!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: "",
+    routingKey: "fila1",
+    body: body);
+
+Console.WriteLine($"Mensagem enviada: {message}");
